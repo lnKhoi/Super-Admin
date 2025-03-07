466466d4a3959cc3a86865aae513c2aeb50eb8af
khoilam <khoilam.dev@gmail.com>
2025-03-07 16:57:05 +0700

    custom config final

--- app/apis/brand.ts
+++ app/apis/brand.ts
@@ -6,7 +6,6 @@ import {
 export const getBrands = () => {
     return getData(`/api/v1/super-admin/brands`)
 }
-
 export const deleteBrandFromConfig = (id: string, brandId: string) => {
     return patchData(`/api/v1/super-admin/${id}/configuration?status=remove-brand`, { brandId: [brandId] })
 }
