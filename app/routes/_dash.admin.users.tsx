74ad9afe08a35fcb03de10e36d042382187a0895
khoilam <khoilam.dev@gmail.com>
2025-03-17 10:56:45 +0700

    super admin logout

--- app/routes/_dash.admin.users.tsx
+++ app/routes/_dash.admin.users.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import Header from '~/components/layout/Header';
+
+import { MetaFunction } from '@remix-run/cloudflare';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Users" },
+  ];
+};
+
+
 function Users() {
   return (
     <div>
-      
+      <Header title='Users' />
     </div>
   )
 }
